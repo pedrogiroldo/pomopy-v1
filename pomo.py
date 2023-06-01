ba1a53a0667974c08b5fe17af6b85c331bb2d440
Pedro Giroldo <pedrogiroldo40@gmail.com>
2023-06-01 17:32:31 -0300

    funçao de timer

--- pomo.py
+++ pomo.py
@@ -1,24 +1,44 @@
 import time
 
+
 def pomodoro():
-    pomodoro_duration = 25  # duração de um pomodoro em minutos
-    short_break_duration = 5  # duração da pausa curta em minutos
-    long_break_duration = 15  # duração da pausa longa em minutos
+    pomodoro_duration = int(
+        input("Duração do pomodoro: ")
+    )  # duração de um pomodoro em minutos
+    short_break_duration = int(
+        input("Duração da pausa curta: ")
+    )  # duração da pausa curta em minutos
+    long_break_duration = int(
+        input("Duração da pausa longa: ")
+    )  # duração da pausa longa em minutos
     pomodoros_completed = 0  # contador de pomodoros concluídos
 
     while True:
         print(f"Pomodoro {pomodoros_completed + 1}: Trabalhando...")
-        time.sleep(pomodoro_duration * 60)  # converte para segundos
+        for remaining in range(pomodoro_duration * 60, 0, -1):
+            minutes = remaining // 60
+            seconds = remaining % 60
+            print(f"{minutes:02d}:{seconds:02d}", end="\r")
+            time.sleep(1)
 
         pomodoros_completed += 1
         if pomodoros_completed % 4 == 0:
             print(f"Pausa longa de {long_break_duration} minutos.")
-            time.sleep(long_break_duration * 60)
+            for remaining in range(long_break_duration * 60, 0, -1):
+                minutes = remaining // 60
+                seconds = remaining % 60
+                print(f"{minutes:02d}:{seconds:02d}", end="\r")
+                time.sleep(1)
         else:
             print(f"Pausa curta de {short_break_duration} minutos.")
-            time.sleep(short_break_duration * 60)
+            for remaining in range(short_break_duration * 60, 0, -1):
+                minutes = remaining // 60
+                seconds = remaining % 60
+                print(f"{minutes:02d}:{seconds:02d}", end="\r")
+                time.sleep(1)
 
         print("Pomodoro concluído! Reiniciando...\n")
+
 
 # Executa o cronômetro Pomodoro
 pomodoro()
