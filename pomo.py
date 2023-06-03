11775a14123f0b64bda92a3783abf77ed99b3fcb
Pedro Giroldo <pedrogiroldo40@gmail.com>
2023-06-03 13:33:15 -0300

    style changes

--- pomo.py
+++ pomo.py
@@ -10,92 +10,74 @@ def play_sound():
         )
     elif platform.system() == "Darwin":
         os.system("afplay done.wav")
-    else: # Linux
+    else:  # Linux
         os.system("aplay done.wav")
 
 
-def clearTerminal():
+def clear_terminal():
     if platform.system() == "Windows":
         os.system("cls")  # Clear the terminal in Windows
     elif platform.system() == "Darwin" or platform.system() == "Linux":
         os.system("clear")  # Clear the terminal in macOS and Linux
 
 
+def format_time(minutes, seconds):
+    return f"{minutes:02d}:{seconds:02d}"
+
+
+def get_input(prompt):
+    return input(prompt).strip().lower()
+
+
+def start_break(duration, break_type):
+    clear_terminal()
+    print(f"\n{break_type} break of {duration} minutes.")
+    play_sound()
+
+    for remaining in range(duration * 60, 0, -1):
+        minutes = remaining // 60
+        seconds = remaining % 60
+        print(f"Time remaining: {format_time(minutes, seconds)}", end="\r")
+        time.sleep(1)
+
+    print("\n")
+    clear_terminal()
+
+
 def pomodoro():
+    clear_terminal()
+    print("=== Pomodoro Timer ===")
+
     pomodoro_duration = int(input("Pomodoro duration (minutes): "))
     short_break_duration = int(input("Short break duration (minutes): "))
     long_break_duration = int(input("Long break duration (minutes): "))
     pomodoros_completed = 0
 
     while True:
-        clearTerminal()
+        clear_terminal()
+        pomodoros_completed += 1
 
-        print(f"Pomodoro {pomodoros_completed + 1}: Working...")
+        print(f"Pomodoro {pomodoros_completed}: Working...")
         for remaining in range(pomodoro_duration * 60, 0, -1):
             minutes = remaining // 60
             seconds = remaining % 60
-            print(f"{minutes:02d}:{seconds:02d}", end="\r")
+            print(f"Time remaining: {format_time(minutes, seconds)}", end="\r")
             time.sleep(1)
 
-        pomodoros_completed += 1
         if pomodoros_completed % 4 == 0:
-            clearTerminal()
-
-            print("\n")
-            play_sound()
-            print(f"Pomodoro completed! Long break of {long_break_duration} minutes.")
-
-            clearTerminal()
-
-            choice = input("Enter 'y' to start the break or 'n' to exit: ")
-            clearTerminal()
-
-            if choice.lower() == "n":
-                print("Exiting the Pomodoro timer...")
-                return
-
-            for remaining in range(long_break_duration * 60, 0, -1):
-                minutes = remaining // 60
-                seconds = remaining % 60
-                print(f"{minutes:02d}:{seconds:02d}", end="\r")
-                time.sleep(1)
+            start_break(long_break_duration, "Long")
         else:
-            clearTerminal()
+            start_break(short_break_duration, "Short")
 
-            print("\n")
-            play_sound()
-            print(f"Pomodoro completed! Short break of {short_break_duration} minutes.")
+        choice = get_input("Enter 'y' to start the next session or 'n' to exit: ")
 
-            clearTerminal()
-
-            choice = input("Enter 'y' to start the break or 'n' to exit: ")
-            clearTerminal()
-
-            if choice.lower() == "n":
-                print("Exiting the Pomodoro timer...")
-                return
-
-            for remaining in range(short_break_duration * 60, 0, -1):
-                minutes = remaining // 60
-                seconds = remaining % 60
-                print(f"{minutes:02d}:{seconds:02d}", end="\r")
-                time.sleep(1)
-
-        print("\n")
-
-        clearTerminal()
-
-        play_sound()
-        choice = input("Enter 'y' to continue or 'n' to exit: ")
-        clearTerminal()
-
-        if choice.lower() == "n":
+        if choice == "n":
             print("Exiting the Pomodoro timer...")
             return
 
-        clearTerminal()
-
+        clear_terminal()
         print("Restarting...\n")
+        time.sleep(1)
 
 
 # Run the Pomodoro timer
